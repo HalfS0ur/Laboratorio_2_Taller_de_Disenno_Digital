2a2ac6e375512686e20dd3234815b08b3ed56de0
Kendall Rivera Solano <124763075+HalfS0ur@users.noreply.github.com>
2024-04-17 21:24:59 -0600

    Update Top.sv

--- PLL_IP_core/Top.sv
+++ PLL_IP_core/Top.sv
@@ -4,16 +4,13 @@ module Top(
     input logic CLK_100MHZ,
     output logic led_po
     );
-    logic clk_100MHz;
+
     logic clk_10MHz;
-    logic led;
     
-  clk_wiz_0 reloj
-   (
-    // Clock out ports
-    .clk_10MHz(clk_10MHz),     // output clk_10MHz
-   // Clock in ports
-    .clk_100MHz(CLK_100MHZ));      // input clk_100MHz
+    clk_wiz_0 reloj(
+    .clk_10MHz(clk_10MHz),
+    .clk_100MHz(CLK_100MHZ)
+    );  
     
     reloj_10MHz prueba(
     .clk_i(clk_10MHz),
