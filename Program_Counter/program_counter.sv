43cc63f31703aec50321491205b0b7a7fcce27c2
Kendall Rivera Solano <124763075+HalfS0ur@users.noreply.github.com>
2024-04-19 16:51:28 -0600

    Update program_counter.sv

--- Program_Counter/program_counter.sv
+++ Program_Counter/program_counter.sv
@@ -11,26 +11,26 @@ module program_counter #(parameter ANCHO = 16)(
     logic [ANCHO-1:0] pc_actual = 0;
     logic [ANCHO-1:0] pc_siguiente = 0;
     
-    always @(posedge clk_i) begin
+    always_ff @(posedge clk_i) begin
         case(pc_op_i)
             'b00: begin
-                pc_actual = 0;
-                pc_siguiente = 0;
+                pc_actual <= 0;
+                pc_siguiente <= 0;
             end
             
             'b01: begin
-                pc_actual = pc_actual;
-                pc_siguiente = 0;
+                pc_actual <= pc_actual;
+                pc_siguiente <= 0;
             end
             
             'b10: begin
-                pc_actual = pc_actual + 4;
-                pc_siguiente = 0;
+                pc_actual <= pc_actual + 4;
+                pc_siguiente <= 0;
             end
             
             'b11: begin
-                pc_siguiente = pc_actual + 4;
-                pc_actual = pc_i;
+                pc_siguiente <= pc_actual + 4;
+                pc_actual <= pc_i;
             end
         endcase
     end
