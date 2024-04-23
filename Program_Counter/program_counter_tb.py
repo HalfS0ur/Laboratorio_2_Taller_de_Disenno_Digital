1fac1945700f59944723844480cbc9a35d928cd4
HalfS0ur <riveras.kendall99@gmail.com>
2024-04-23 14:11:01 -0600

    Prueba cuenta y hold

--- Program_Counter/program_counter_tb.py
+++ Program_Counter/program_counter_tb.py
@@ -20,5 +20,29 @@ async def prueba_reset(dut):
         dut.pc_op_i.value = operacion
         await FallingEdge(dut.clk_i)
 
-        assert dut.pc_o.value == pc_actual, f"El valor de LED_po esperado es {pc_actual}, se recibió {dut.pc_o.value}"
-        assert dut.pcinc_o.value == pc_actual, f"El valor de LED_po esperado es {pc_salto}, se recibió {dut.pcinc_o.value}"
+        assert dut.pc_o.value == pc_actual, f"El valor de pc_o esperado es {pc_actual}, se recibió {dut.pc_o.value}"
+        assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado es {pc_salto}, se recibió {dut.pcinc_o.value}"
+
+
+@cocotb.test()
+async def prueba_cuenta_hold(dut):
+    pc_actual = 4
+    pc_salto = 0000000000000000
+    operacion = 2
+    for test in range (512):  #32768
+        await cocotb.start(generar_reloj_10MHz(dut))
+        dut.pc_op_i.value = operacion
+        await FallingEdge(dut.clk_i)
+        #pc_actual = pc_actual + 4
+
+        assert dut.pc_o.value == pc_actual, f"El valor de pc_o esperado es {pc_actual}, se recibió {dut.pc_o.value}"
+        assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado es {pc_salto}, se recibió {dut.pcinc_o.value}"
+
+        pc_actual = pc_actual + 4
+
+    dut.pc_op_i.value = 1
+    pc_hold = pc_actual - 4
+    await Timer (20, units = 'us')
+    assert dut.pc_o.value == pc_hold, f"El valor de pc_o esperado al detener la cuenta es {pc_hold}, se recibió {dut.pc_o.value}"
+    assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado es {pc_salto}, se recibió {dut.pcinc_o.value}"
+
