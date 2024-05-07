0c5daa15abdfce5a63658b9934a256836acd8ce4
HalfS0ur <riveras.kendall99@gmail.com>
2024-05-06 21:20:35 -0600

    prueba 1

--- Interfaz_Teclado_Hex/interfaz_teclado_hex_tb.py
+++ Interfaz_Teclado_Hex/interfaz_teclado_hex_tb.py
@@ -15,26 +15,26 @@ async def reiniciar_modulo(dut):
     await Timer (200, units = 'ns')
     dut.reset_i.value = 0
 
-def mapeo_key_encoding(segmento):
+def mapeo_key_encoding(valor_teclado):
         mapeo = {
-        '0000': int('1000000', 2),  # 0
-        '0001': int('1111001', 2),  # 1
-        '0010': int('0100100', 2),  # 2
-        '0011': int('0110000', 2),  # 3
-        '0100': int('0011001', 2),  # 4
-        '0101': int('0010010', 2),  # 5
-        '0110': int('0000010', 2),  # 6
-        '0111': int('1111000', 2),  # 7
-        '1000': int('0000000', 2),  # 8
-        '1001': int('0010000', 2),  # 9
-        '1010': int('0001000', 2),  # A
-        '1011': int('0000011', 2),  # B
-        '1100': int('1000110', 2),  # C
-        '1101': int('0100001', 2),  # D
-        '1110': int('0000110', 2),  # E
-        '1111': int('0001110', 2)   # F
+        '0000': int('0111', 2),  # 7
+        '0001': int('1010', 2),  # A
+        '0010': int('1001', 2),  # 9
+        '0011': int('1000', 2),  # 8
+        '0100': int('0100', 2),  # 4
+        '0101': int('1011', 2),  # B
+        '0110': int('0110', 2),  # 6
+        '0111': int('0101', 2),  # 5
+        '1000': int('0001', 2),  # 1
+        '1001': int('1100', 2),  # C
+        '1010': int('0011', 2),  # 3
+        '1011': int('0010', 2),  # 2
+        '1100': int('1111', 2),  # F
+        '1101': int('1101', 2),  # D
+        '1110': int('1110', 2),  # E
+        '1111': int('0000', 2)   # 0
     }
-        return mapeo.get(segmento, int('1111111', 2))
+        return mapeo.get(valor_teclado, int('1111', 2))
 
 @cocotb.test()
 async def prueba_columna_11(dut):
@@ -42,14 +42,17 @@ async def prueba_columna_11(dut):
     await reiniciar_modulo(dut)
     await RisingEdge(dut.clk_i)
 
-    dato_codificador = 0
+    dato_codificador = 3
     valor_contador = 3
 
     valor_concatenado = (valor_contador << 2) | dato_codificador
+    valor_binario = f'{valor_concatenado:04b}'
+    valor_codificado = mapeo_key_encoding(valor_binario)
 
     while dut.cuenta_dos_bits_o.value != 0:
         await RisingEdge(dut.clk_i)
 
     dut.pulso_teclas_pi.value = 1
     dut.dato_codificador_i.value = dato_codificador
-    await RisingEdge(dut.data_available_o)
+    await FallingEdge(dut.data_available_o)
+    assert dut.dato_codificado_o.value == valor_codificado
