d4eb409c832dce7f062802ea280bbc73ed5e59b4
Kendall Rivera Solano <124763075+HalfS0ur@users.noreply.github.com>
2024-05-07 14:04:14 -0600

    Add files via upload

---
+++ Unidad_de_Calculo/registro_paralelo.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module registro_paralelo(
+    input logic          clk_i,
+    input logic          we_i,
+    input logic          reset_i,
+    input logic  [15:0]  data_i,
+    
+    output logic [15:0]  data_o
+    );
+    
+    logic [15:0] data = 0;
+    
+    always_ff @(posedge clk_i or posedge reset_i) begin
+        if (reset_i) begin
+            data <= 0;
+        end
+        
+        else if (we_i) begin
+            data <= data_i;
+        end
+    end
+    
+    assign data_o = data;
+endmodule
--- Unidad_de_Calculo/top.sv
+++ Unidad_de_Calculo/top.sv
@@ -4,13 +4,18 @@ module top(
     input logic clk_i,
     input logic reset_i,
     input logic sw_i,
+    input logic key_detect_i,
+    input logic [3:0] teclado_i,
     
+    output logic [3:0] an_o,
+    output logic [6:0] seg_o,
     output logic [15:0] led_o
     );
     
     logic seleccion_mux;
     logic we_regfile;
     logic alu_flag;
+    logic we_7_segmentos;
     logic [3:0] alu_control;
     logic [4:0] addr_rd;
     logic [4:0] addr_rs1;
@@ -19,12 +24,13 @@ module top(
     logic [15:0] rs1;
     logic [15:0] rs2;
     logic [15:0] alu_result;
+    logic [15:0] dato_7_segmentos;
     
     fsm_control control(
         .clk_i(clk_i),         
         .rst_i(reset_i),         
-        .teclado_i(),      
-        .key_detect_i(),   
+        .teclado_i(teclado_i),      
+        .key_detect_i(key_detect_i),   
         .sw_i(sw_i),                                
         .mux_o(seleccion_mux),          
         .addr_rs1_o(addr_rs1),     
@@ -34,12 +40,12 @@ module top(
         .alucont_o(alu_control),      
         .flag_in(alu_flag),        
         .led_o(led_o),          
-        .we_7seg_o()      
+        .we_7seg_o(we_7_segmentos)      
     );
     
     mux_2_1 mux_2_1(
         .seleccion_i(seleccion_mux),
-        .entrada0_i(), 
+        .entrada0_i(teclado_i), 
         .entrada1_i(alu_result), 
         .salida_o(data_in)    
     );
@@ -62,6 +68,22 @@ module top(
         .ALUflagin_i(alu_flag),  
         .ALUcontrol_i(alu_control),                     
         .ALUresult_o(alu_result)   
+    );
+    
+    registro_paralelo registro_7_segmentos(
+        .clk_i(clk_i),   
+        .we_i(we_7_segmentos),     
+        .reset_i(reset_i),  
+        .data_i(rs1),                  
+        .data_o(dato_7_segmentos)   
+    );
+    
+    display_7_segmentos display_7_segmentos(
+        .clk_i(clk_i),
+        .reset_i(reset_i),
+        .dato_i(dato_7_segmentos),
+        .an_o(an_o),
+        .seg_o(seg_o)
     );
     
 endmodule
