6a1c60d7230a3434dd3898cc194685f92f4fe728
HalfS0ur <riveras.kendall99@gmail.com>
2024-06-15 18:49:23 -0600

    Clock improvements

--- PLL_IP_core/reloj_10MHz_tb.py
+++ PLL_IP_core/reloj_10MHz_tb.py
@@ -1,21 +1,18 @@
 import cocotb
 from cocotb.triggers import FallingEdge, Timer
+from cocotb.clock import Clock
 
-async def generar_reloj_10MHz (dut):
-    while True:
-        dut.clk_i.value = 0
-        await Timer (100, units = 'ns')
-        dut.clk_i.value = 1
-        await Timer (100, units = 'ns')
+numero_pruebas = 2**8
 
 @cocotb.test()
-async def verificacion_reloj (dut):
+async def verificacion_reloj(dut):
+    clock = Clock(dut.clk_i, 100, 'ns')
     valor_LED = 0
-    for i in range (20):
-        await cocotb.start(generar_reloj_10MHz(dut))
-        await Timer (20, units = 'us')
+    await cocotb.start(clock.start())
+
+    for i in range (numero_pruebas):   
+        await Timer (10, units = 'us')
         valor_LED = 1 if valor_LED == 0 else 0
         await FallingEdge(dut.clk_i)
 
-        dut._log.info("my_signal_1 is %s", dut.LED_po.value)
         assert dut.LED_po.value == valor_LED, f"El valor de LED_po esperado es {valor_LED}, se recibió {dut.LED_po.value}"
