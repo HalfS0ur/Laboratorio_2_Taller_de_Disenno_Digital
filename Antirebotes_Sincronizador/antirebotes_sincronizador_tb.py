cec46722d937cb9c4e04ee0dddfeb8d87b6cd1db
HalfS0ur <riveras.kendall99@gmail.com>
2024-06-15 19:23:21 -0600

    Formato testbench y makefile

--- Antirebotes_Sincronizador/antirebotes_sincronizador_tb.py
+++ Antirebotes_Sincronizador/antirebotes_sincronizador_tb.py
@@ -1,35 +1,37 @@
 import cocotb
 from cocotb.triggers import FallingEdge, Timer
+from cocotb.clock import Clock
 import random
 
-async def generar_reloj_10MHz (dut):
-    while True:
-        dut.clk_i.value = 0
-        await Timer (100, units = 'ns')
-        dut.clk_i.value = 1
-        await Timer (100, units = 'ns')
+periodo_boton = 4
+numero_pruebas = 2**8
+
+async def reset_dut(dut, duration=100):
+    dut.reset_pi.value = 1
+    dut.boton_pi.value = 0
+    await Timer(duration, units='us')
+    dut.reset_pi.value = 0
+    await Timer(duration, units='us')
+    await FallingEdge(dut.clk_i)
 
 @cocotb.test()
 async def prueba_contador(dut):
-    cuenta = 0
-    await cocotb.start(generar_reloj_10MHz(dut))
-    dut.reset_pi.value = 1
+    clock = Clock(dut.clk_i, 100, 'ns')
+    await cocotb.start(clock.start())
+    await reset_dut(dut)
+    
     dut.boton_pi.value = 0
-    await Timer (100, units = 'us')
-    dut.reset_pi.value = 0
-    await Timer (100, units = 'us')
+    cuenta = 0
 
-    for test in range (128):
+    for test in range(numero_pruebas):
         tiempo_pulso = random.randint(1, 16)
         dut.boton_pi.value = 1
-        await Timer (tiempo_pulso, units = 'us')
+        await Timer(tiempo_pulso, units='us')
         dut.boton_pi.value = 0
-        await Timer (10, units = 'us')
+        await Timer(10, units='us')
 
-        if tiempo_pulso >= 7:
-            cuenta = cuenta + 1
-        else:
-            cuenta = cuenta
+        if tiempo_pulso >= periodo_boton:
+            cuenta += 1
 
         await FallingEdge(dut.clk_i)
         assert dut.conta_o.value == cuenta, f"El valor del contador esperado es {cuenta}, se recibió {dut.conta_o.value}"
