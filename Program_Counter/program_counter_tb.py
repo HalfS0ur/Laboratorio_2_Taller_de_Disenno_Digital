3d198d026a6235ba8519c1a1fbaa5d2b98f1a5fa
HalfS0ur <riveras.kendall99@gmail.com>
2024-06-15 20:58:11 -0600

    Formato testbench, pruebas extra, formato makefile

--- Program_Counter/program_counter_tb.py
+++ Program_Counter/program_counter_tb.py
@@ -1,74 +1,116 @@
 import cocotb
 from cocotb.triggers import FallingEdge, Timer, RisingEdge
-
+from cocotb.clock import Clock
 import random
 
-async def generar_reloj_10MHz (dut):
-    while True:
-        dut.clk_i.value = 0
-        await Timer (100, units = 'ns')
-        dut.clk_i.value = 1
-        await Timer (100, units = 'ns')
+NUMERO_PRUEBAS = 2**16
+
+async def reiniciar_modulo(dut):
+    dut.pc_op_i.value = 0
+    dut.pc_i.value = 0
+    await Timer(200, units = 'ns')
 
 
 @cocotb.test()
 async def prueba_reset(dut):
+    clock = Clock(dut.clk_i, 100, 'ns')
+    await cocotb.start(clock.start())
+
     pc_actual = 0
     pc_salto  = 0
     operacion = 0
-    for test in range (16):
-        await cocotb.start(generar_reloj_10MHz(dut))
-        dut.pc_op_i.value = operacion
-        await FallingEdge(dut.clk_i)
 
+    for test in range(128):
+        dut.pc_op_i.value = operacion
+
+        await FallingEdge(dut.clk_i)
         assert dut.pc_o.value == pc_actual, f"El valor de pc_o esperado es {pc_actual}, se recibió {dut.pc_o.value}"
         assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado es {pc_salto}, se recibió {dut.pcinc_o.value}"
 
 
 @cocotb.test()
-async def prueba_cuenta_hold(dut):
+async def prueba_cuenta(dut):
+    clock = Clock(dut.clk_i, 100, 'ns')
+    await cocotb.start(clock.start())
+
     pc_actual = 4
     pc_salto = 0
     operacion = 2
-    for test in range (5):  #32768
-        await cocotb.start(generar_reloj_10MHz(dut))
+
+    await(reiniciar_modulo(dut))
+
+    for test in range(NUMERO_PRUEBAS): 
         dut.pc_op_i.value = operacion
+
         await FallingEdge(dut.clk_i)
-        #pc_actual = pc_actual + 4
-
         assert dut.pc_o.value == pc_actual, f"El valor de pc_o esperado es {pc_actual}, se recibió {dut.pc_o.value}"
-        assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado es {pc_salto}, se recibió {dut.pcinc_o.value}"
+        assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado es {pc_salto}, se recibió {dut.pcinc_o.value}" 
 
-        pc_actual = pc_actual + 4
+        pc_actual = (pc_actual + 4) & 0xFFFF
 
-    dut.pc_op_i.value = 1
-    pc_hold = pc_actual - 4
-    await Timer (20, units = 'us')
-    assert dut.pc_o.value == pc_hold, f"El valor de pc_o esperado al detener la cuenta es {pc_hold}, se recibió {dut.pc_o.value}"
-    assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado al detener la cuenta es {pc_salto}, se recibió {dut.pcinc_o.value}"
 
-    await Timer (4, units = 'us')
-    dut.pc_op_i.value = 0
-    await Timer (4, units = 'us')
+@cocotb.test()
+async def prueba_hold(dut):
+    clock = Clock(dut.clk_i, 100, 'ns')
+    await cocotb.start(clock.start())
+
+    pc_actual = 0
+    pc_salto = 0
+    operacion = 1
+
+    await(reiniciar_modulo(dut))
+
+    for test in range(NUMERO_PRUEBAS):
+        dut.pc_op_i.value = operacion
+
+        await Timer(400, units = 'ns')
+        assert dut.pc_o.value == pc_actual, f"El valor de pc_o esperado es {pc_actual}, se recibió {dut.pc_o.value}"
+        assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado es {pc_salto}, se recibió {dut.pcinc_o.value}" 
+
+        dut.pc_op_i.value = 2
+        await Timer(100, units = 'ns')
+        pc_actual = (pc_actual + 4) & 0xFFFF
     
 
 @cocotb.test()
 async def prueba_salto(dut):
-    pc_actual = 0
-    pc_salto = 0
-    dir_salto = 0
+    clock = Clock(dut.clk_i, 100, 'ns')
+    await cocotb.start(clock.start())
+
+    dir_salto = 4
+    pc_salto = dir_salto
     operacion = 3
-    for test in range (256):
-        await cocotb.start(generar_reloj_10MHz(dut))
+
+    await(reiniciar_modulo(dut))
+
+    for test in range(NUMERO_PRUEBAS):
         dut.pc_op_i.value = operacion
-        dir_salto = random.randint(0, 65536)
         dut.pc_i.value = dir_salto
-        await RisingEdge(dut.clk_i)
+
         await FallingEdge(dut.clk_i)
+        assert dut.pc_o.value == dir_salto, f"El valor de pc_o esperado luego del salto es es {dir_salto}, se recibió {dut.pc_o.value}"
+        assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado es {pc_salto}, se recibió {dut.pcinc_o.value}" 
 
-        assert dut.pc_o.value == dir_salto, f"El valor de pc_o esperado al realizar el salto es {dir_salto}, se recibió {dut.pc_o.value}"
+        dir_salto = (dir_salto + 4) & 0xFFFF
+        pc_salto = dir_salto
 
-        pc_salto = pc_actual + 4
-        pc_actual = dir_salto
 
-        assert dut.pcinc_o.value == pc_salto, f"El valor de pcinc_o esperado al realizar el salto es {pc_salto}, se recibió {dut.pcinc_o.value}"
+@cocotb.test()
+async def prueba_salto_aleatorio(dut):
+    clock = Clock(dut.clk_i, 100, 'ns')
+    await cocotb.start(clock.start())
+
+    operacion = 3
+
+    await(reiniciar_modulo(dut))
+
+    for test in range(NUMERO_PRUEBAS):
+        direccion_salto = random.randint(0, (NUMERO_PRUEBAS - 1))
+        pc_inc = ((dut.pc_i.value + 4) & 0xFFFF)
+
+        dut.pc_op_i.value = operacion
+        dut.pc_i.value = direccion_salto
+
+        await FallingEdge(dut.clk_i)
+        assert dut.pc_o.value == direccion_salto, f"El valor de pc_o esperado luego del salto es es {direccion_salto}, se recibió {dut.pc_o.value}"
+        assert dut.pcinc_o.value == pc_inc, f"El valor de pcinc_o esperado es {pc_inc}, se recibió {dut.pcinc_o.value}"
