26795946efc639f7fc113767cd97313cdf7855e9
HalfS0ur <riveras.kendall99@gmail.com>
2024-08-23 18:07:16 -0600

    Removed testing signals

--- Antirebotes_Sincronizador/top.sv
+++ Antirebotes_Sincronizador/top.sv
@@ -4,26 +4,26 @@ module top(
     input logic clk_i,
     input logic boton_pi,
     input logic reset_pi,
-    output logic [7:0]conta_o,
-    output logic boton_o
+    output logic [7:0]conta_o
     );
     
-    logic boton_debounce_o;
+    logic boton_debounce;
+    logic boton_sync;
     
     debouncer debounce(
     .clk(clk_i),
     .boton_pi(boton_pi),
-    .boton_debounce_o(boton_debounce_o));
+    .boton_debounce_o(boton_debounce));
     
     sincronizador sync(
     .clk(clk_i),
-    .boton_i(boton_debounce_o),
-    .boton_o(boton_o));
+    .boton_i(boton_debounce),
+    .boton_o(boton_sync));
     
     contador cuenta(
     .clk(clk_i),
     .reset_i(reset_pi),
-    .boton_i(boton_o),
+    .boton_i(boton_sync),
     .conta_o(conta_o)); 
 
 endmodule
